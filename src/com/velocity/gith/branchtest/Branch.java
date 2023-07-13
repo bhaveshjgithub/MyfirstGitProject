349e755220b799820b5d3be4e2794367d55e74a6
Bhavesh Jivrakh <bhaveshjivrakh@gmail.com>
2023-07-13 08:22:36 +0530

    This is ArrayList Added

---
+++ src/com/velocity/gith/branchtest/Branch.java
@@ -0,0 +1,22 @@
+package com.velocity.gith.branchtest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Branch {
+
+	public static void main(String[] args) {
+		//This is ArrayList Cheak
+		
+		List arr = new ArrayList();
+		arr.add("Bhavesh");
+		arr.add(10);
+		arr.add('j');
+		arr.add(10);
+		arr.remove(3);
+		
+		System.out.println(arr);
+
+	}
+
+}
